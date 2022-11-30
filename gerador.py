adcdb47b60468f1a29bf9d6013994be8ac4fd5bb
PHMac20XX2 <phmestudos@gmail.com>
2022-11-30 14:47:09 -0300

    Primeira Versão (g_senha.1.0)

---
+++ gerador.py
@@ -0,0 +1,10 @@
+from random import choice
+import string
+
+t_senha = int(input("Quantidade de Caracteres: "))
+carac = string.ascii_letters + string.digits + string.punctuation
+sec_senha = ''
+for i in range(t_senha):
+    sec_senha += choice (carac)
+
+print("Senha: ", sec_senha)
