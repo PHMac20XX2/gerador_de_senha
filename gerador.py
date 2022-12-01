bc76d04c431f35671d9d6b707c3dbf333024ae56
PHMac20XX2 <phmestudos@gmail.com>
2022-12-01 12:24:13 -0300

    att 1.0

--- gerador.py
+++ gerador.py
@@ -1,5 +1,13 @@
+import time
 from random import choice
 import string
+
+print ("Gerador de Senha by PHMac20XX2")
+
+def pull():
+    print("")
+
+pull()
 
 t_senha = int(input("Quantidade de Caracteres: "))
 carac = string.ascii_letters + string.digits + string.punctuation
@@ -7,4 +15,10 @@ sec_senha = ''
 for i in range(t_senha):
     sec_senha += choice (carac)
 
-print("Senha: ", sec_senha)
+print("Gerando senha...")
+time.sleep(1.5)
+print(f'''
+             Senha Gerada!
+                ( {sec_senha} )          ''')
+
+
